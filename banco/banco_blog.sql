975f96846b6f054a31cc3bb98f0cd311ae57725a
bianegrisoli <bianca.negrisoli@aluno.ifsp.edu.br>
2023-08-29 09:33:16 -0300

    banco

--- banco/banco_blog.sql
+++ banco/banco_blog.sql
@@ -1,26 +1,37 @@
-DROP DATABASE IF EXISTS blog;
 CREATE DATABASE blog;
 USE blog;
 
-CREATE TABLE usuario (
-    id int NOT NULL AUTO_INCREMENT,
-    nome varchar(50) NOT NULL, 
-    email varchar(255) NOT NULL,
-    senha varchar(60) NOT NULL,
-    data_criacao datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    ativo tinyint NOT NULL DEFAULT '0',
-    ADM tinyint NOT NULL DEFAULT '0',
-    PRIMARY KEY (id)
+CREATE TABLE usuario(
+    id int not null auto_increment,
+    nome varchar(50) not null,
+    email varchar(255) not null,
+    senha varchar(60) not null,
+    data_criacao datetime not null default current_timestamp,
+    ativo tinyint not null default '0',
+    adm tinyint not null default '0',
+    primary key(id)
 );
 
-CREATE TABLE post (
-    id int NOT NULL AUTO_INCREMENT,
-    titulo varchar(255) NOT NULL,
-    texto text NOT NULL,
-    usuario_id int NOT NULL,
-    data_criacao datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    data_postagem datetime NOT NULL,
-    PRIMARY KEY (id),
-    KEY fk_post_usuario_idx (usuario_id),
-    CONSTRAINT fk_post_usuario FOREIGN KEY (usuario_id)REFERENCES usuario (id)
+create table post(
+    id int not null auto_increment,
+    titulo varchar(255) not null,
+    texto text not null,
+    usuario_id int not null,
+    data_criacao datetime not null default current_timestamp,
+    data_postagem datetime not null,
+    primary key(id),
+    key fk_post_usuario_idx(usuario_id),
+    constraint fk_post_usuario foreign key(usuario_id) references usuario(id)
+);
+
+create table avaliacao (
+    id int not null auto_increment,
+    nota int not null,
+    comentario varchar(255) not null,
+    usuario_id int not null,
+    post_id int not null,
+    data_criacao datetime not null default current_timestamp,
+    primary key(id),
+    constraint fk_avaliacao_usuario foreign key (usuario_id) references usuario (id),
+    constraint fk_avaliacao_post foreign key (post_id) references post (id)
 );
