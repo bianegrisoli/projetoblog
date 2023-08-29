9fefbdd50f0371753bf42422535c7c15bcec8990
bianegrisoli <bianca.negrisoli@aluno.ifsp.edu.br>
2023-08-29 09:31:39 -0300

    blog

--- tests/mysql_avaliacao_test.php
+++ tests/mysql_avaliacao_test.php
@@ -1,33 +1,32 @@
 <?php
-
 require_once '../includes/funcoes.php';
 require_once '../core/conexao_mysql.php';
 require_once '../core/sql.php';
 require_once '../core/mysql.php';
 
-insert_teste('10', 'bom', '123', '1', '15/08');
+insert_teste ('8', 'Esse post alegrou meu dia','3','3');
 buscar_teste();
-update_teste(38, 'murilo','silva@gmail.com');
+update_teste(1, '1', 'Pessimo','4','2');
 buscar_teste();
 
-//Teste inserção banco de dados
-function insert_teste($nota, $comentario, $usuario, $post, $data) : void
-{
-    $dados = ['nota'=> $nota, 'comentario' => $comentario, 'usuario' => $usuario, 'post' => $post, 'data' => $data];
-    insere('usuario',$dados);
+function insert_teste($nota, $comentario, $usuario_id, $post_id): void{
+
+    $dados = ['nota' => $nota, 'comentario' => $comentario, 'usuario_id' => $usuario_id, 'post_id' => $post_id]; 
+    insere ('avaliacao', $dados);
+}
+function buscar_teste(): void{
+    $avaliacoes = buscar('avaliacao',['id', 'nota', 'comentario', 'usuario_id', 'post_id'], [], '');
+    print_r($avaliacoes);
 }
 
-//Teste select banco de dados
-function buscar_teste() : void
-{
-    $usuarios = buscar('usuario', [ 'id','nota','comentario'],[],'');
-    print_r($usuarios);
+function update_teste($id, $nota, $comentario, $usuario_id, $post_id): void{
+
+    $dados = ['nota' => $nota, 'comentario' => $comentario, 'usuario_id' => $usuario_id, 'post_id' => $post_id];
+    $criterio = [['id', '=', $id]];
+    atualiza('avaliacao', $dados, $criterio);
 }
 
-//Teste update banco de dados
-function update_teste($id, $nota, $comentario) : void
-{
-    $dados = ['nota' => $nota, 'comentario' => $comentario];
-    $criterio = [['id', '-', $id]];
-    atualiza('usuario',$dados,$criterio);
-}
+
+
+
+?>
